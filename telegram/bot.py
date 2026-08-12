51308186d0e8de2e326c1ea09ffe7f62912ec233
ljm970-cmyk <ljm970@gmail.com>
2026-08-12 10:26:06 +0900

    Update bot.py

--- telegram/bot.py
+++ telegram/bot.py
@@ -5,6 +5,7 @@ from telegram.handlers import TelegramHandlers
 
 logger = setup_logger()
 
+
 class KiwoomTelegramBot:
     def __init__(self):
         config = load_config()
@@ -12,13 +13,15 @@ class KiwoomTelegramBot:
         self.handlers = TelegramHandlers()
 
     def run(self):
-        logger.info("텔레그램 봇 시작")
+        logger.info("무적 좀비봇 시작")
         application = Application.builder().token(self.token).build()
 
+        # ⭐ 핵심 명령어 등록
         application.add_handler(CommandHandler("start", self.handlers.start))
+        application.add_handler(CommandHandler("update", self.handlers.cmd_update))  # ⭐ NEW
+
         application.add_handler(CallbackQueryHandler(self.handlers.handle_callback))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handlers.handle_message))
 
+        logger.info("데몬 폴링 시작...")
         application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-from telegram import Update
